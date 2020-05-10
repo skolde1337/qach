46ba1d0ed4d7d7465ef1ed46411ca5ec927e41b6
Skolde322 <49067072+Skolde322@users.noreply.github.com>
2020-05-10 19:18:02 +0300

    Автотест заполнение text

--- q2.py
+++ q2.py
@@ -8,11 +8,11 @@ try:
     browser.get(link)
 
     input1 = browser.find_element_by_tag_name('input')
-    input1.send_keys("Ivan")
+    input1.send_keys("Sasha")
     input2 = browser.find_element_by_name('last_name')
     input2.send_keys("Petrov")
     input3 = browser.find_element_by_class_name('form-control.city')
-    input3.send_keys("Smolensk")
+    input3.send_keys("Moscow")
     input4 = browser.find_element_by_id("country")
     input4.send_keys("Russia")
     button = browser.find_element_by_css_selector("button.btn")
